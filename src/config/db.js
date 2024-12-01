07ccb72af34afe1a1f1ce8aadb694701a080f1b2
AlexisRamon <111154480+AlexisRamon@users.noreply.github.com>
2024-11-30 21:33:43 -0700

    db update

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,13 @@
 const mongoose = require("mongoose");
 
-let isConnected;
-
 const connectDB = async () => {
-  if (isConnected) {
-    console.log("Ya conectado a MongoDB.");
-    return;
-  }
-
-  try {
-    const db = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    isConnected = db.connections[0].readyState;
-    console.log("Conectado a MongoDB:", db.connections[0].name);
-  } catch (error) {
-    console.error("Error al conectar a MongoDB:", error.message);
-    throw error;
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("Conexión a MongoDB exitosa.");
+    } catch (error) {
+        console.error("Error al conectar a MongoDB:", error.message);
+        process.exit(1); // Salir si no se puede conectar
+    }
 };
 
 module.exports = connectDB;
